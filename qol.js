409ea0c79c8c82d5afb762cf1c8760995ebba7fb
chaodziejg <mariuszploszynski@o2.pl>
2026-01-03 18:10:55 +0100

    Remove codychat 9.0

---
+++ qol.js
@@ -0,0 +1,27 @@
+// stop profile music autoplay
+window.loadProMusic = function () {
+    const audio = document.getElementById('promusic');
+    const player = document.getElementById('proplayer');
+
+    if (!audio) return;
+
+    const audioSource = audio.getAttribute('data-pmusic');
+    if (!audioSource) return;
+
+    audio.src = audioSource;
+    audio.load();
+
+    const playIcon = document.querySelector('.proplayer_play');
+    const beatIcon = document.querySelector('.proplayer_beat');
+
+    if (playIcon) {
+        playIcon.setAttribute('src', 'default_images/icons/play.svg');
+    }
+    if (beatIcon) {
+        beatIcon.setAttribute('src', 'default_images/profile/wavestop.gif');
+    }
+
+    if (player) {
+        player.setAttribute('data-state', '0');
+    }
+};
